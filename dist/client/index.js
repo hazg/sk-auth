41fd9ac8510fbfaf32ff634c5fa17d2e5730aa47
hazg <hazg@mail.ru>
2022-01-09 13:22:28 +0300

    remove dist

--- dist/client/index.js
+++
@@ -1,3 +0,0 @@
-export { signIn } from './signIn.js';
-export { signOut } from './signOut.js';
-//# sourceMappingURL=index.js.map
--- dist/client/signIn.d.ts
+++
@@ -1,5 +0,0 @@
-interface SignInConfig {
-    redirectUrl?: string;
-}
-export declare function signIn(provider: string, data?: any, config?: SignInConfig): Promise<any>;
-export {};
